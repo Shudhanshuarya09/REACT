12d17fdf02cd8b3896f9698986b6dd21a21ec026
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-29 19:05:36 +0530

    Second component added

--- 2-react-component/create-first-comp/src/App.jsx
+++ 2-react-component/create-first-comp/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Button from "./Button";
+import Hello from "../Hello";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  <h1>
-    Lets start
-  </h1>
-    )
+    <>
+      <h1>Lets start</h1>
+      <Hello />
+    </>
+  );
 }
 
-export default App
+export default App;
