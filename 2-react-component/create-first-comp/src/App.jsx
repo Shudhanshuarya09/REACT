49b846de436d1192e14aa17e6e7806f0efb3fce6
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-29 19:23:36 +0530

    Random number added to app.jsx

--- 2-react-component/create-first-comp/src/App.jsx
+++ 2-react-component/create-first-comp/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Button from "./Button";
 import Hello from "../Hello";
+import Random from "../Random";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
     <>
       <h1>Lets start</h1>
       <Hello />
+      <Random />
     </>
   );
 }
