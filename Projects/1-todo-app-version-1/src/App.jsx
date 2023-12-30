3c537ca74ec875dacc500ccd50fd9855ca07b028
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-30 18:38:46 +0530

    Todo-App Intialize

---
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <center class="todo-container">
+      Todo App
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">col-4</div>
+          <div class="col-4">col-4</div>
+          <div class="col-2">col-8</div>
+        </div>
+      </div>
+    </center>
+  );
+}
+
+export default App;
