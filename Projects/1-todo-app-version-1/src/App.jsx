1ca5609fb3310eec751580a4ac07c550d4d2144d
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-30 19:57:56 +0530

    TodoItem 1&2 Added to App.jsx file

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,39 +1,19 @@
 import AddTodo from "./Components/AddTodo";
 import AppName from "./Components/AppName";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2";
 function App() {
   return (
     <center class="todo-container">
       <AppName/>
       <AddTodo/>
-      <AddTodo/>
+      <TodoItem1/>
+      <TodoItem2/>
+      
 
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            Buy Milk
-          </div>
-          <div class="col-4">
-            5/12/2023
-          </div>
-          <div class="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
       
       
       
-        <div class="row">
-          <div class="col-6">
-            Go to college
-          </div>
-          <div class="col-4">
-            5/12/2023
-          </div>
-          <div class="col-2">
-          <button type="button" class="btn btn-danger">Delete</button>
-          </div>
-        </div>
-      </div>
     </center>
   );
 }
