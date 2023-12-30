c1ffd655b0e960bdeed65caa74467eab43df1c0c
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-31 01:02:43 +0530

    CSS added to app

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -2,18 +2,18 @@ import AddTodo from "./Components/AddTodo";
 import AppName from "./Components/AppName";
 import TodoItem1 from "./Components/TodoItem1";
 import TodoItem2 from "./Components/TodoItem2";
+import "./App.css";
+
+
+
+
 function App() {
   return (
-    <center class="todo-container">
-      <AppName/>
-      <AddTodo/>
-      <TodoItem1/>
-      <TodoItem2/>
-      
-
-      
-      
-      
+    <center class="todo container" >
+      <AppName />
+      <AddTodo />
+      <TodoItem1 />
+      <TodoItem2 />
     </center>
   );
 }
