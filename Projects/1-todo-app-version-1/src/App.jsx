cca872eb6673f6185c19ca9ff1fa573bbfdd1d22
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2023-12-30 19:43:18 +0530

    AddTodo Component make

--- Projects/1-todo-app-version-1/src/App.jsx
+++ Projects/1-todo-app-version-1/src/App.jsx
@@ -1,23 +1,11 @@
+import AddTodo from "./Components/AddTodo";
 import AppName from "./Components/AppName";
 function App() {
   return (
     <center class="todo-container">
       <AppName/>
-      <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Todo Here" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
-      </div>
+      <AddTodo/>
+      <AddTodo/>
 
       <div class="container text-center">
         <div class="row">
@@ -31,9 +19,9 @@ function App() {
           <button type="button" class="btn btn-danger">Delete</button>
           </div>
         </div>
-      </div>
       
-      <div class="container text-center">
+      
+      
         <div class="row">
           <div class="col-6">
             Go to college
