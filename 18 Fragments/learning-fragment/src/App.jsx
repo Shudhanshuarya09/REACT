a0fe02dcac4fda4961b2595d06f85ebd0856879a
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-01-06 21:55:12 +0530

    React.Fragment tag used

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,9 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
+import React from "react";
+
 
 function App() {
   
 
   return (
-    <h1>Fragments</h1>
+    <React.Fragment>
+      <h1>Healthy Food</h1>
+    <ul class="list-group">
+  <li class="list-group-item active" aria-current="true">An active item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+    </React.Fragment>
   )
 };
 
