b2dce7df769ef73747a1e8f3560cd084fdc26fdb
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-01-06 22:33:42 +0530

    Map Method and short syntax of Fragment used

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -1,23 +1,41 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css"
-import React from "react";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css"
+// import React from "react";
 
+// function App() {
+
+//   return (
+//     <React.Fragment>
+//       <h1>Healthy Food</h1>
+//     <ul class="list-group">
+//   <li class="list-group-item active" aria-current="true">An active item</li>
+//   <li class="list-group-item">A second item</li>
+//   <li class="list-group-item">A third item</li>
+//   <li class="list-group-item">A fourth item</li>
+//   <li class="list-group-item">And a fifth one</li>
+// </ul>
+//     </React.Fragment>
+//   )
+// };
+
+// export default App;
+
+//same code written uper will be write in using short syntax of FRAGMENT and MAP METHOD concept
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
-  
 
+  let foodItems = ["CHICKEN","EGG","FISH","BUTTER","SPINACH","PULSES","PANEER","GREEN VEGETABLES"]
   return (
-    <React.Fragment>
+    <>
       <h1>Healthy Food</h1>
-    <ul class="list-group">
-  <li class="list-group-item active" aria-current="true">An active item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-</ul>
-    </React.Fragment>
-  )
-};
+      <ul class="list-group">
+        {foodItems.map((item) => (<li class="list-group-item">{item}</li>))}
+      </ul>
+    </>
+  );
+}
 
 export default App;
