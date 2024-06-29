8e2d29499d8b4933c405af9448cdee89a9006e03
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-06-29 13:11:17 +0530

    CSS Module concept used

--- 18 Fragment/learning fragment/src/component/Item.jsx
+++ 18 Fragment/learning fragment/src/component/Item.jsx
@@ -1,10 +1,11 @@
+import styles from "./Item.module.css";
 const Item = (props) => {
 
   //let { foodItem} = props;           concept of the array de-structuring
  // const Item = (foodItem) => {       concept next level de-structuring
   return (
-    <li  className="list-group-item">
-      {props.foodItem}
+    <li  className={' ${stylesKgItem} list-group-item'}>
+      <span className="Kg-span">{foodItem}</span>
     </li>
   );
 };
