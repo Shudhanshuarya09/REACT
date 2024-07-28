45b4963261ed561739c58a21d307832ba38f9cde
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-28 21:56:21 +0530

    Synthetic event concept used

--- 18 Fragment/learning fragment/src/component/Item.jsx
+++ 18 Fragment/learning fragment/src/component/Item.jsx
@@ -1,13 +1,21 @@
 import FoodItem from "./FoodItem";
 import styles from "./Item.module.css";
 const Item = ({ foodItems }) => {
+
+const handleBuyButtonClicked = (event) => {
+  console.log(event);
+  console.log(`${foodItems} being bought`);
+};
+
+
   //let { foodItem} = props;           concept of the array de-structuring
   // const Item = (foodItem) => {       concept next level de-structuring
   return (
     <li className={` ${styles["Kg-Item"]} list-group-item`}>
       <span className={styles["Kg-span"]}>{foodItems}</span>
       <button className={`${styles.button} btn btn-info `}
-      onClick = {() => console.log(`${foodItems} being bought `)}
+      //onClick = {() => console.log(`${foodItems} being bought `)}
+      onClick={(event) => handleBuyButtonClicked(event)}         //synthetic event concept
       
       
       >Buy</button>
