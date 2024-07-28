ddb20b22529403926de0d0f945e297eaaa3719fb
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-28 10:46:18 +0530

    passing childern concept

--- 18 Fragment/learning fragment/src/App.jsx
+++ 18 Fragment/learning fragment/src/App.jsx
@@ -1,17 +1,17 @@
- import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
-import FoodItems from "./component/FoodItem";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import FoodItem from "./component/FoodItem";
 import ErrorMessage from "./component/ErrorMessage";
+import Container from "./component/Container";
 
 function App() {
- 
-let foodItems = ["Dal", "Rice", "Spinach", "Fruits", "Vegetable"];
+  let foodItems = ["Dal", "Rice", "Spinach", "Fruits", "Vegetable"];
   return (
-    <>
-    <h1>Healthy Food</h1>
-    <ErrorMessage items = {foodItems}></ErrorMessage>
-    <FoodItems items = {foodItems}></FoodItems>
-    </>   
+    <Container>
+      <h1 className="food-heading">Healthy Food</h1>
+      <ErrorMessage items={foodItems}></ErrorMessage>
+      <FoodItem items={foodItems}></FoodItem>
+    </Container>
   );
 }
 
