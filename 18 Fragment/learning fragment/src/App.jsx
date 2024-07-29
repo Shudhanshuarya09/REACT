0d92352d0a3e01d3ecd6d4a6be5e06f285c6572f
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-29 21:54:00 +0530

    State managne(useState)

--- 18 Fragment/learning fragment/src/App.jsx
+++ 18 Fragment/learning fragment/src/App.jsx
@@ -4,27 +4,38 @@ import FoodItems from "./component/FoodItems";
 import ErrorMessage from "./component/ErrorMessage";
 import Container from "./component/Container";
 import FoodInput from "./component/FoodInput";
+import { useState } from "react";
 
 function App() {
   let foodItems = ["Dal", "Rice", "Spinach", "Fruits", "Vegetable"];
 
-   const handleOnChange = (event) => {
+  // let textToShow = textStateArr[0];             
+  // let setTextState = textStateArr[1];
+  let { textToShow, setTextState } = useState("Food Item Entered by user");              //De-structuring
+  console.log(`Current value of textState:${textToShow}`);
+
+  const handleOnChange = (event) => {
     console.log(event.target.value);
-   };
+    setTextState(event.target.value);
+  };
 
   return (
-     <> // fragments use beacuse it allow to write two container in app.jsx
-    <Container>
-      <h1 className="food-heading">Healthy Food</h1>
-      <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodInput handleOnChange={handleOnChange}></FoodInput>
-      <FoodItems items={foodItems}></FoodItems>
-    </Container>
-
-    <Container>
-      <p>Above is the list of the healthy foods for the godd health and well being</p>
-    </Container>
-  </>
+    <>
+      // fragments use beacuse it allow to write two container in app.jsx
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <p>{textToShow}</p>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>
+          Above is the list of the healthy foods for the godd health and well
+          being
+        </p>
+      </Container>
+    </>
   );
 }
 
