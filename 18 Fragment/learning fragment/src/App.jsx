21065f2356fc46a59d12d33b81bd7bbe4c6bc075
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-29 17:55:15 +0530

    Passing function via props

--- 18 Fragment/learning fragment/src/App.jsx
+++ 18 Fragment/learning fragment/src/App.jsx
@@ -1,19 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import FoodItem from "./component/FoodItem";
+import FoodItems from "./component/FoodItems";
 import ErrorMessage from "./component/ErrorMessage";
 import Container from "./component/Container";
 import FoodInput from "./component/FoodInput";
 
 function App() {
   let foodItems = ["Dal", "Rice", "Spinach", "Fruits", "Vegetable"];
+
+   const handleOnChange = (event) => {
+    console.log(event.target.value);
+   };
+
   return (
      <> // fragments use beacuse it allow to write two container in app.jsx
     <Container>
       <h1 className="food-heading">Healthy Food</h1>
       <ErrorMessage items={foodItems}></ErrorMessage>
-      <FoodInput></FoodInput>
-      <FoodItem items={foodItems}></FoodItem>
+      <FoodInput handleOnChange={handleOnChange}></FoodInput>
+      <FoodItems items={foodItems}></FoodItems>
     </Container>
 
     <Container>
--- 18 Fragment/learning fragment/src/component/Item.jsx
+++ 18 Fragment/learning fragment/src/component/Item.jsx
@@ -1,24 +1,23 @@
-import FoodItem from "./FoodItem";
+// import FoodItem from "./FoodItems";
 import styles from "./Item.module.css";
-const Item = ({ foodItems }) => {
-
-const handleBuyButtonClicked = (event) => {
-  console.log(event);
-  console.log(`${foodItems} being bought`);
-};
-
+const Item = ({ foodItems, handleBuyButton }) => {
+  // const handleBuyButtonClicked = (event) => {
+  //   console.log(event);
+  //   console.log(`${foodItems} being bought`);
+  // };
 
   //let { foodItem} = props;           concept of the array de-structuring
   // const Item = (foodItem) => {       concept next level de-structuring
   return (
     <li className={` ${styles["Kg-Item"]} list-group-item`}>
       <span className={styles["Kg-span"]}>{foodItems}</span>
-      <button className={`${styles.button} btn btn-info `}
-      //onClick = {() => console.log(`${foodItems} being bought `)}
-      onClick={(event) => handleBuyButtonClicked(event)}         //synthetic event concept
-      
-      
-      >Buy</button>
+      <button
+        className={`${styles.button} btn btn-info `}
+        //onClick = {() => console.log(`${foodItems} being bought `)}
+        onClick={handleBuyButton} //synthetic event concept
+      >
+        Buy
+      </button>
     </li>
   );
 };
