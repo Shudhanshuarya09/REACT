fe740e4a73201b73f003ef1b0465294de24737de
Shudhanshuarya09 <shudhanshuarya111@gmail.com>
2024-07-30 23:35:04 +0530

    Code edit

--- 18 Fragment/learning fragment/src/App.jsx
+++ 18 Fragment/learning fragment/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <>
-      // fragments use beacuse it allow to write two container in app.jsx
+       {/* fragments use beacuse it allow to write two container in app.jsx */}
       <Container>
         <h1 className="food-heading">Healthy Food</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
